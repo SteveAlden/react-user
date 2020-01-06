735c87807b5c89818fd9c1183e628374f6686577
Steve Alden <stevealden.77@gmail.com>
2020-01-06 12:17:45 +0530

    Updates

---
+++ precache-manifest.8845a6281d724c72e3ec157ff389c1f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb6692bd073dfd5425ba15008742dc5",
+    "url": "/react-userinfo/index.html"
+  },
+  {
+    "revision": "0d77be9fe9e96083a3fd",
+    "url": "/react-userinfo/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5632529bba6d01476bb9",
+    "url": "/react-userinfo/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "0d77be9fe9e96083a3fd",
+    "url": "/react-userinfo/static/js/2.64249d7b.chunk.js"
+  },
+  {
+    "revision": "142c6f7a4ce1dda5888c98ba1a4a271f",
+    "url": "/react-userinfo/static/js/2.64249d7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5632529bba6d01476bb9",
+    "url": "/react-userinfo/static/js/main.b54f7a8e.chunk.js"
+  },
+  {
+    "revision": "7fe6a9c18fe08bfb17ef",
+    "url": "/react-userinfo/static/js/runtime-main.721aa63d.js"
+  }
+]);
