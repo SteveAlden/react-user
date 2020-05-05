ec7068a5ba63ce60be86cd10f9065d8451ccd9a1
Steve Alden <stevealden.77@gmail.com>
2020-05-05 12:28:23 +0530

    .

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
           <Switch>
             <Route
               exact
-              path='/'
+              path='/home'
               render={(props) => (
                 <React.Fragment>
                   <Container style={{ paddingTop: '60px' }}>
